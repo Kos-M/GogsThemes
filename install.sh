232f88313fe6dfec73f724c3a2bf6fa57ba5d570
lowking <lowking@sina.cn>
2021-02-02 15:08:30 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mkdir -p ../custom/templates/inject/
 mkdir -p ../custom/public/css/
+rm ../custom/templates/inject/head.tmpl
 echo '<link rel="stylesheet" href="/css/themes/dark_theme.css">' >> ../custom/templates/inject/head.tmpl
 cp -r ./themes/ ../custom/public/css/
 cp -r ./img/ ../custom/public/
